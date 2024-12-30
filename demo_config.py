2c257793f965edbaba6b6ea746ec1af52ab308fc
Adam Karvonen <adam.karvonen@gmail.com>
2024-12-30 17:52:11 +0000

    Update pythia gated sparsity penalties

--- demo_config.py
+++ demo_config.py
@@ -71,7 +71,7 @@ SPARSITY_PENALTIES = {
     "EleutherAI/pythia-70m-deduped": SparsityPenalties(
         standard=[0.01, 0.02, 0.03, 0.04, 0.05, 0.06],
         p_anneal=[0.02, 0.03, 0.035, 0.04, 0.05, 0.075],
-        gated=[0.1, 0.3, 0.5, 0.7, 0.9, 1.1],
+        gated=[0.012, 0.018, 0.024, 0.04, 0.06, 0.08],
     ),
     "google/gemma-2-2b": SparsityPenalties(
         standard=[0.012, 0.015, 0.02, 0.03, 0.04, 0.06],
